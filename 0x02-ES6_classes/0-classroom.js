8dabca1b5d53748c7cc10d3b5d586c2e50eaf3fe
gabbrenn <gabvladimirbrenn@gmail.com>
2024-05-31 06:26:51 +0000

    update

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,16 +1,6 @@
-// Define and export the ClassRoom class
 export default class ClassRoom {
-  // Private field for maximum students size
-  #maxStudentsSize;
-
-  // Constructor to initialize the maximum students size
   constructor(maxStudentsSize) {
-    this.#maxStudentsSize = maxStudentsSize;
-  }
-
-  // Getter method for the maximum students size
-  getMaxStudentsSize() {
-    return this.#maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
 
